4c6a78934714e895a986399f1416ec6a998831be
Romain Martinez <martinez.staps@gmail.com>
2023-11-11 10:49:34 -0500

    remove useless comment

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,5 +2,4 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- map <leader>fs to save file
 vim.keymap.set("n", "<leader>fs", "<cmd>w<cr><esc>", { desc = "Save File" })
