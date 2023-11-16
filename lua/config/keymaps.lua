0d5ed82f1a49da028fdbcd7997d5cec65508c882
Romain Martinez <martinez.staps@gmail.com>
2023-11-16 10:27:58 -0500

    add keymaps for ctr+d/u with zz

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,6 @@
 -- Add any additional keymaps here
 
 vim.keymap.set("n", "<leader>fs", "<cmd>w<cr><esc>", { desc = "Save File" })
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Center cursor after moving down half-page" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Center cursor after moving up half-page" })
