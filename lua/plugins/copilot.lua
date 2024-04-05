8d256d8e44185932b243a2471dc1e3e1163caa65
Romain Martinez <martinez.staps@gmail.com>
2024-04-05 10:49:06 -0400

    add copilot

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    opts = {
+      filetypes = {
+        yaml = true,
+        markdown = true,
+        help = true,
+        gitcommit = true,
+        gitrebase = true,
+      },
+    },
+  },
+}
