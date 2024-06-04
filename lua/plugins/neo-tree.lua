2ff85956ae5bced99e80de341b16aaa815268a0e
Romain Martinez <martinez.staps@gmail.com>
2024-06-04 11:06:18 -0400

    Show hidden files by default (can always toggle with H)

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,6 +1,13 @@
 return {
   {
     "nvim-neo-tree/neo-tree.nvim",
-    opts = { window = { position = "right", width = 30 } },
+    opts = {
+      window = { position = "right", width = 30 },
+      filesystem = {
+        filtered_items = {
+          visible = true, -- Shows hidden files
+        },
+      },
+    },
   },
 }
