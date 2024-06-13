396dc2da0d8ba4e5a4d54c2d758f3849ee23de10
Romain Martinez <martinez.staps@gmail.com>
2024-06-13 10:25:44 -0400

    venv selector for new laptop

--- lua/plugins/venv-selector.lua
+++ lua/plugins/venv-selector.lua
@@ -1,14 +1,42 @@
+-- return {
+--   {
+--     "linux-cultist/venv-selector.nvim",
+--     cmd = "VenvSelect",
+--     opts = {
+--       search_workspace = false,
+--       path = "",
+--       parents = 0,
+--       anaconda_base_path = "/opt/homebrew/Caskroom/miniconda",
+--       anaconda_envs_path = "/opt/homebrew/Caskroom/miniconda/base/envs/",
+--     },
+--     keys = { { "<leader>cc", "<cmd>:VenvSelectCached<cr>", desc = "Select Cached VirtualEnv" } },
+--   },
+-- }
 return {
   {
     "linux-cultist/venv-selector.nvim",
-    cmd = "VenvSelect",
-    opts = {
-      search_workspace = false,
-      path = "",
-      parents = 0,
-      anaconda_base_path = "/Users/romainm/miniconda",
-      anaconda_envs_path = "/Users/romainm/miniconda/envs/",
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "mfussenegger/nvim-dap",
+      "mfussenegger/nvim-dap-python",
+      { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
     },
-    keys = { { "<leader>cc", "<cmd>:VenvSelectCached<cr>", desc = "Select Cached VirtualEnv" } },
+    lazy = false,
+    branch = "regexp",
+    config = function()
+      require("venv-selector").setup({
+        settings = {
+          search = {
+            anaconda_base = {
+              command = "fd -H '^python[0-9.]*$' --type f --type x /opt/homebrew/Caskroom/miniconda | grep -E '/(base|envs/.+)/bin/python[0-9.]*$'",
+              type = "anaconda",
+            },
+          },
+        },
+      })
+    end,
+    keys = {
+      { ",v", "<cmd>VenvSelect<cr>" },
+    },
   },
 }
