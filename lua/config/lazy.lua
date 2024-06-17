2c6ca3e70ae46c9453cf54ce63257a0cd99ca204
Romain Martinez <martinez.staps@gmail.com>
2024-06-17 10:09:22 -0400

    add toml and json languages

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -19,7 +19,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.coding.copilot-chat" },
     { import = "lazyvim.plugins.extras.dap.core" },
-    -- { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.lang.toml" },
+    { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     -- import/override with your plugins
