7df7093a47f04dd15f0d5144fc40841f342fda57
Romain Martinez <martinez.staps@gmail.com>
2024-07-03 09:59:39 -0400

    add function to copy all buffers to clipboard

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,3 +8,23 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Center cursor after moving dow
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Center cursor after moving up half-page" })
 
 vim.keymap.set("n", "<leader>dD", ":w<CR>:split term://python %<CR>i", { desc = "Run file without debugging" })
+
+-- Function to copy all buffers' content with filenames to clipboard
+local function copy_all_buffers_to_clipboard()
+  local buffers = vim.api.nvim_list_bufs()
+  local result = ""
+  for _, bufnr in ipairs(buffers) do
+    if vim.api.nvim_buf_is_loaded(bufnr) and vim.api.nvim_buf_is_valid(bufnr) then
+      local filename = vim.api.nvim_buf_get_name(bufnr)
+      local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+      local content = table.concat(lines, "\n")
+      if result ~= "" then
+        result = result .. "\n\n"
+      end
+      result = result .. "File: " .. filename .. "\n\n" .. content
+    end
+  end
+  vim.fn.setreg("+", result)
+  print("All buffer contents copied to clipboard")
+end
+vim.keymap.set("n", "<leader>bc", copy_all_buffers_to_clipboard, { desc = "Copy all buffers to clipboard" })
