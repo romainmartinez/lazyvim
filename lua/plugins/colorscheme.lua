69653600d55f994b9a241b9a118327109c2bd0f1
Romain Martinez <martinez.staps@gmail.com>
2025-11-19 16:40:35 -0500

    add theme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "storm" },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
+}
