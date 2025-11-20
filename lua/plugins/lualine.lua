75b7983785d6dee1bfba2ba70c1044393a229eeb
Romain Martinez <martinez.staps@gmail.com>
2025-11-20 16:41:14 -0500

    disable lualine progress

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,6 +4,9 @@ return {
     sections = {
       -- remove the clock from the bottom
       lualine_z = {},
+      lualine_y = {
+        { "location", padding = { left = 0, right = 1 } },
+      },
     },
   },
 }
