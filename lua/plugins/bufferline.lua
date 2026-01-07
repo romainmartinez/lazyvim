101c0cff6b1af7b9cfb13d99aec55154e71669c4
Romain Martinez <martinez.staps@gmail.com>
2026-01-07 10:46:26 -0500

    chore(bufferline): configure buffer navigation shortcuts and disable icons

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,7 +1,22 @@
 return {
   "akinsho/bufferline.nvim",
+  keys = {
+    { "<leader>1", "<cmd>BufferLineGoToBuffer 1<cr>", desc = "Go to buffer 1" },
+    { "<leader>2", "<cmd>BufferLineGoToBuffer 2<cr>", desc = "Go to buffer 2" },
+    { "<leader>3", "<cmd>BufferLineGoToBuffer 3<cr>", desc = "Go to buffer 3" },
+    { "<leader>4", "<cmd>BufferLineGoToBuffer 4<cr>", desc = "Go to buffer 4" },
+    { "<leader>5", "<cmd>BufferLineGoToBuffer 5<cr>", desc = "Go to buffer 5" },
+    { "<leader>6", "<cmd>BufferLineGoToBuffer 6<cr>", desc = "Go to buffer 6" },
+    { "<leader>7", "<cmd>BufferLineGoToBuffer 7<cr>", desc = "Go to buffer 7" },
+    { "<leader>8", "<cmd>BufferLineGoToBuffer 8<cr>", desc = "Go to buffer 8" },
+    { "<leader>9", "<cmd>BufferLineGoToBuffer 9<cr>", desc = "Go to buffer 9" },
+  },
   opts = {
     options = {
+      numbers = function(opts)
+        return opts.raise(opts.ordinal)
+      end,
+      show_buffer_icons = false,
       groups = {
         items = {
           require("bufferline.groups").builtin.pinned:with({ icon = "*" }),
