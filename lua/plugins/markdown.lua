cbeec23c4cb9a9efa3e187f1e2558360a00a789a
Romain Martinez <martinez.staps@gmail.com>
2026-03-03 09:19:50 -0500

    feat(markdown): auto-detect HTML in selection to use correct prettier parser

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -11,7 +11,19 @@ return {
     keys = {
       {
         "<leader>ct",
-        ":'<,'>!prettier --parser markdown<cr>",
+        function()
+          -- Exit visual mode to set '< and '> marks
+          vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "x", false)
+          local first_line = vim.fn.getline("'<")
+          local is_html = first_line:match("^%s*<") ~= nil
+          local parser = is_html and "vue" or "markdown"
+          vim.notify("prettier --parser " .. parser, vim.log.levels.INFO)
+          if is_html then
+            vim.cmd("'<,'>!{ echo '<template>'; cat; echo '</template>'; } | prettier --parser vue | sed '1d;$d'")
+          else
+            vim.cmd("'<,'>!prettier --parser markdown")
+          end
+        end,
         mode = "v",
         ft = "markdown",
         desc = "Format Markdown Selection",
