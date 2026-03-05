69bcfbc3cc02c38c7c8a5574dba885ad57cd0cc8
Romain Martinez <martinez.staps@gmail.com>
2026-03-05 14:42:39 -0500

    refactor: disable lazyvim wrap+spell group instead of overriding spell separately

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -5,7 +5,9 @@
 -- with `vim.api.nvim_create_autocmd`
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
--- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+-- Disable default wrap + spell for markdown/text/gitcommit filetypes
+
+vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
 -- Auto-save when leaving a buffer or exiting Neovim
 local function augroup(name)
@@ -20,10 +22,3 @@ vim.api.nvim_create_autocmd({ "BufLeave", "VimLeave", "FocusLost" }, {
   end,
 })
 
--- disable spell checking in markdown
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "markdown", "txt" },
-  callback = function()
-    vim.opt_local.spell = false
-  end,
-})
