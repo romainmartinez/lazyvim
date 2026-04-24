eabfe2c9eb60302a43613a185a54c11f94d034ac
Romain Martinez <martinez.staps@gmail.com>
2026-04-24 09:25:30 -0400

    feat(flash): enable n/N navigation after jump

---
+++ lua/plugins/flash.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/flash.nvim",
+  opts = {
+    modes = {
+      search = { enabled = false },
+    },
+    jump = {
+      history = true,
+      register = true,
+      nohlsearch = false,
+    },
+  },
+}
