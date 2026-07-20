f64b2ee2692d1d233eb94bff5c8c0fe0f4e7d429
Romain Martinez <martinez.staps@gmail.com>
2026-07-20 10:47:37 -0400

    feat(picker): toggle hidden and ignored files in one keymap

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -8,6 +8,22 @@ return {
           height = 0,
         },
       },
+      picker = {
+        actions = {
+          toggle_hidden_ignored = function(picker)
+            picker.opts.hidden = not picker.opts.hidden
+            picker.opts.ignored = picker.opts.hidden
+            picker:find({ refresh = true })
+          end,
+        },
+        win = {
+          input = {
+            keys = {
+              ["<a-.>"] = { "toggle_hidden_ignored", mode = { "i", "n" } },
+            },
+          },
+        },
+      },
     },
     keys = {
       { "<C-g>", function() Snacks.lazygit() end, desc = "Lazygit" },
