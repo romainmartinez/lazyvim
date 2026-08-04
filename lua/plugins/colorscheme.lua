0cfb0ed3269b698ee9a21a7ea2abe4ba0889f77c
Romain Martinez <martinez.staps@gmail.com>
2026-08-04 10:58:56 -0400

    feat: highlight markdown bold in orange

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,7 +3,12 @@ return {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
-    opts = { style = "storm" },
+    opts = {
+      style = "storm",
+      on_highlights = function(highlights, colors)
+        highlights["@markup.strong"] = { fg = colors.orange, bold = true }
+      end,
+    },
   },
   { "rose-pine/neovim" },
   {
